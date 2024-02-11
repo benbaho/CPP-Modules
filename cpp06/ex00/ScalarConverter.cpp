b81284a31e4fcda6d2e2241f8937a038d4b0bfc4
Bahadır Durmuş <mrbahadir25@gmail.com>
2024-02-11 17:56:24 +0300

    Update ScalarConverter.cpp

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: bdurmus <bdurmus@student.42kocaeli.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 22:41:39 by bdurmus           #+#    #+#             */
-/*   Updated: 2024/02/09 22:18:02 by bdurmus          ###   ########.fr       */
+/*   Updated: 2024/02/11 16:59:48 by bdurmus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,25 +34,14 @@ void printTypes(std::string _char, std::string _int, std::string _double, std::s
         std::cout << "double: " << _double << std::endl;
 }
 
-int ScalarConverter::checkNonChars(const std::string &tmp){
-    if (tmp.size() == 1)
-    {
-        std::cout << "char: " << "Non Displayable"  << std::endl;
-        std::cout << "int : " << static_cast<int>(tmp[0]) << std::endl;
-        std::cout << "float: " << static_cast<float>(tmp[0]) << ".0f"<< std::endl;
-        std::cout << "double: " << static_cast<double>(tmp[0]) << ".0" <<std::endl;
 
-        return (1);
-    }
-    return (0);
-}       
 
 int ScalarConverter::checkNan(const std::string &tmp){
     if (!tmp.compare("-inff") || !tmp.compare("+inff") || !tmp.compare("nanf"))
     {
         float a = atof(tmp.c_str());
-        std::cout << "char: " << "impossible"  << std::endl;
         std::cout << "int : " << "impossible" << std::endl;
+        std::cout << "char: " << "impossible"  << std::endl;
         std::cout << "float: " << static_cast<float>(a) <<  "f" << std::endl;
         std::cout << "double: " << static_cast<double>(a) <<std::endl;
         return (1);
@@ -72,33 +61,50 @@ int ScalarConverter::checkNan(const std::string &tmp){
 int ScalarConverter::checkInt(const std::string &tmp){
     if (tmp.size() != 1)
     {
-        if (INT_MAX < atol(tmp.c_str()) || INT_MIN > atol(tmp.c_str()))
+        if (INT_MAX < atol(tmp.c_str()) || INT_MIN > atol(tmp.c_str())){
+            std::cout << "char: " << "impossible" << std::endl;
             std::cout << "int : " << "impossible" << std::endl;
-        int a = atoi(tmp.c_str());
-        std::cout << "char: " << static_cast<char>(a) << std::endl;
-        std::cout << "int : " << a << std::endl;
-        std::cout << "float: " << static_cast<float>(a) <<  "f" << std::endl;
-        float b = static_cast<float>(a);
-        std::cout << "int : " << static_cast<int>(b) << std::endl;
-        std::cout << "double: " << static_cast<double>(a) <<std::endl;
+            std::cout << "float: " << static_cast<float>(atol(tmp.c_str())) <<  "f" << std::endl;
+            std::cout << "double: " << static_cast<double>(atol(tmp.c_str())) <<std::endl;
 
+        }
+        else
+        {
+            int a = atoi(tmp.c_str());
+            if (isprint(a))
+                std::cout << "char: " << static_cast<char>(a) << std::endl;
+            else
+                std::cout << "char: " << "Non displayable" << std::endl;
+            std::cout << "int : " << a << std::endl;
+            std::cout << "float: " << static_cast<float>(a) <<  "f" << std::endl;
+            std::cout << "double: " << static_cast<double>(a) <<std::endl;
+        }
         return (1);
     }
     return (0);
 }
 
 int ScalarConverter::checkAscii(const std::string &tmp){
-    if (tmp.size() == 1 && !isdigit(tmp[0]) && isascii(tmp[0]))
+    if (tmp.size() == 1  && isascii(tmp[0]))
     {
-        std::cout << static_cast<char>(tmp[0]) << std::endl;
-        return (0);
+        char a = tmp[0];
+        if (isprint(a))
+            std::cout << "char: " <<  a << std::endl;
+        else
+            std::cout << "char: " << "Non displayable" << std::endl;
+        std::cout << "int: " << static_cast<int>(a) << std::endl;
+        std::cout << "float: " << static_cast<float>(a) <<  "f" << std::endl;
+        std::cout << "double: " << static_cast<double>(a) <<std::endl;
+        return (1);
     }
-    return (1);
+    return (0);
 }
 
 
 void ScalarConverter::convert(const std::string &number){
-    if ( checkInt(number))
+    if (checkAscii(number) || checkInt(number) )
         return;
+    else    
+        printTypes("impossible", "impossible", "impossible", "impossible");
 
 }
