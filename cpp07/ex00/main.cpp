b726d05acbb7ea6f979b18c4b82846f55acc6e2c
Bahadır Durmuş <mrbahadir25@gmail.com>
2024-02-19 18:13:19 +0300

    Add files via upload

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdurmus <bdurmus@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 17:29:33 by bdurmus           #+#    #+#             */
+/*   Updated: 2024/02/19 17:43:15 by bdurmus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) 
+{
+    int a = 2;
+    int b = 3;
+
+    ::swap( a, b );
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    return 0;
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdurmus <bdurmus@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 17:29:31 by bdurmus           #+#    #+#             */
+/*   Updated: 2024/02/19 18:11:21 by bdurmus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+template<typename T>
+void swap(T &a, T &b){
+    T tmp;
+    
+    tmp = b;
+    b = a;
+    a = tmp;
+}
+
+template<typename T>
+T min(T &a, T &b){
+    if (a < b)
+        return (a);
+    return (b);
+}
+
+template<typename T>
+T max(T &a, T &b){
+    if (a > b)
+        return (a);
+    return (b);
+}
