4ccb3e1cf95057fc97ac5ab2c982dffda853ea44
Bahadır Durmuş <mrbahadir25@gmail.com>
2024-02-29 13:15:24 +0300

    Add files via upload

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -3,28 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bdurmus <bdurmus@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*   By: bdurmus <bdurmus@student.42kocaeli.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 16:55:09 by bdurmus           #+#    #+#             */
-/*   Updated: 2024/02/28 20:25:10 by bdurmus          ###   ########.fr       */
+/*   Updated: 2024/02/29 00:06:45 by bdurmus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int findValue(std::string key, std::string value)
+void checkFile(std::fstream &tmp)
+{
+    std::string line;
+    if (getline(tmp, line).eof())
+    {
+        tmp.close();
+        throw std::invalid_argument("Error: File is empty.");
+    }
+}
+
+void findValue(std::string key, std::string value)
 {
     std::fstream file("data.csv", std::ios::in);
+    std::fstream file1("data.csv", std::ios::in);
     std::string line;
     int i = 0;
 
-    if (!file.is_open() || getline(file,line).eof())
-    {   
-        if (file.is_open())
-            file.close();
-        return (1);
-    }
-    
+    if (!file.is_open())
+        throw std::invalid_argument("Error: Data file is not opened.");
+
+    checkFile(file1);
     std::map<std::string, std::string> btcValues;
     while (std::getline(file, line))
     {   
@@ -41,8 +49,8 @@ int findValue(std::string key, std::string value)
         --it;
         std::cout << key << " => " << value << " = " << atof(value.c_str()) * atof(it->second.c_str()) << std::endl;
     }
+    file1.close();
     file.close();
-    return (0);
 }
 
 int checkKey(std::string key)
@@ -58,18 +66,19 @@ int checkKey(std::string key)
     for (std::vector<std::string>::iterator it = tmp.begin(); it != tmp.end();it++)
     {
         c += it->size();
-        if (it->size() == 4){
+        if (it->size() == 4)
+        {
             if (atoi(it->c_str()) < 0)
                 return (1);
         }
-        else if (it->size() >= 2){
-            if (atoi(it->c_str()) < 1 || atoi(it->c_str()) > 31) 
+        else if (it->size() >= 2)
+        {
+            if ((atoi(it->c_str()) < 1 || atoi(it->c_str()) > 31)) 
             {
                 std::cout << "Error: bad input => " << key << std::endl;
                 return (1);
             }
         }
-        
     }
     if (c != 8)
         return (std::cout << "Error: bad input => " << key << std::endl, 1);
@@ -93,20 +102,33 @@ int checkValue(std::string value)
     return (1);
 }
 
+int checkChars(std::string tmp)
+{
+    std::string chars("0123456789 |-.");
+    for (int i = 0; tmp[i];i++)
+    {
+        if (chars.find(tmp[i]) == std::string::npos)
+        {
+            std::cout << "Error: bad input => " << tmp << std::endl;
+            return (1);
+        }
+    }
+    return (0);
+}
+
 void exec(std::string tmp)
 {
     std::fstream file(tmp, std::ios::in);
+    std::fstream file1(tmp, std::ios::in);
     std::string line;
+
     int i = 0;
     
-    if (!file.is_open() || getline(file, line).eof())
-    {
-        if (file.is_open())
-            file.close();
+    if (!file.is_open())
         throw std::runtime_error("Error: Input file error.");
-    }
     else
     {
+        checkFile(file1);
         while (std::getline(file, line))
         {
             if (i == 0){
@@ -124,20 +146,15 @@ void exec(std::string tmp)
             {
                 std::string key = line.substr(0, pos);
                 std::string value = line.substr(pos + 1);
-                if (!checkKey(key) && !checkValue(value))
-                {
-                    if (findValue(key, value))
-                    {
-                        file.close();
-                        throw std::invalid_argument("Error: Data file error.");
-                    }
-                } 
+                if (!checkChars(line) && !checkKey(key) && !checkValue(value))
+                    findValue(key, value);
             }
             else
                 if (line.size() > 0)
                     std::cout << "Error: bad input => " << line << std::endl;
             i++;
         }      
+        file1.close();
         file.close();
     }
 }
